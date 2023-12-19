914b3d02f740af1b68ce006c9e4c97ff6dff0ae3
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-19 14:52:16 +0300

    Task 2: sorting method

--- src/main/java/com/workintech/grocer/fruitsandvegetables/dao/FruitRepository.java
+++ src/main/java/com/workintech/grocer/fruitsandvegetables/dao/FruitRepository.java
@@ -2,6 +2,11 @@ package com.workintech.grocer.fruitsandvegetables.dao;
 
 import com.workintech.grocer.fruitsandvegetables.entity.Fruit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface FruitRepository extends JpaRepository<Fruit, Integer> {
+    @Query("SELECT f FROM Fruit f ORDER BY f.price ASC")
+    List<Fruit> sortByPrice();
 }
--- src/main/java/com/workintech/grocer/fruitsandvegetables/dao/VegetableRepository.java
+++ src/main/java/com/workintech/grocer/fruitsandvegetables/dao/VegetableRepository.java
@@ -1,7 +1,13 @@
 package com.workintech.grocer.fruitsandvegetables.dao;
 
+import com.workintech.grocer.fruitsandvegetables.entity.Fruit;
 import com.workintech.grocer.fruitsandvegetables.entity.Vegetable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface VegetableRepository extends JpaRepository<Vegetable, Integer> {
+    @Query("SELECT v FROM Vegetable v ORDER BY v.price ASC")
+    List<Fruit> sortByPrice();
 }
