39d6466dd7114c13ae4129adf0ed77d15a5f0bba
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-19 17:07:37 +0300

    Exception handling

--- src/main/java/com/workintech/grocer/fruitsandvegetables/exceptions/GlobalErrorHandler.java
+++ src/main/java/com/workintech/grocer/fruitsandvegetables/exceptions/GlobalErrorHandler.java
@@ -1,5 +1,6 @@
 package com.workintech.grocer.fruitsandvegetables.exceptions;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,5 +11,11 @@ public class GlobalErrorHandler {
     public ResponseEntity<ErrorResponse> handleException(FruitAndVegetableException fruitAndVegetableException) {
         ErrorResponse errorResponse = new ErrorResponse(fruitAndVegetableException.getMessage());
         return new ResponseEntity<>(errorResponse, fruitAndVegetableException.getHttpStatus());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<ErrorResponse> handleException(Exception exception){
+        ErrorResponse errorResponse = new ErrorResponse(exception.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
