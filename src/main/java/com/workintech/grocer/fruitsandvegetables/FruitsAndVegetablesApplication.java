255fc74f52fac45b4b7395dc6c730329a8fd1e8b
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-12-19 14:06:55 +0300

    Cloned

---
+++ src/main/java/com/workintech/grocer/fruitsandvegetables/FruitsAndVegetablesApplication.java
@@ -0,0 +1,13 @@
+package com.workintech.grocer.fruitsandvegetables;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitsAndVegetablesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitsAndVegetablesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/workintech/grocer/fruitsandvegetables/FruitsAndVegetablesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workintech.grocer.fruitsandvegetables;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitsAndVegetablesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
